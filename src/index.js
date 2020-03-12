5660fd7358b13663dd9b966bee91c60f25f28b9c
Hola <hola131098@gmail.com>
2020-03-12 13:14:55 +0400

    feat: morse-decoder upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MORSE_TABLE = {
+const table = {
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var morze = [];
+    var a = '';
+    var letr = '';
+    var lettrs = [];
+    var words = expr.split('*****');
+    for (i=0; i<words.length; i++){
+        for (j = 0; j < words[i].length; j+=2) {
+            lettrs.push(words[i].slice(j, j + 2));
+}
+    }
+
+    for (i=0; i<lettrs.length; i++){
+        if (lettrs[i] == '00'){
+            letr = letr;
+        }
+        if (lettrs[i] == '10'){
+            letr = letr + '.';
+        }
+        if (lettrs[i] == '11'){
+            letr = letr + '-';
+        }
+        if ((i+1) % 5 == 0) {
+            morze.push(letr);
+            letr = ''
+        }
+    }
+    
+    for (l=0; l<morze.length; l++) {
+        letr = morze[l];
+        a = a + table[letr];
+        
+    }
+    return a;
 }
 
 module.exports = {
